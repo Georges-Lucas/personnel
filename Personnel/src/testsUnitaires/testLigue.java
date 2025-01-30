258cd681929a40a70bfced0bea9baa8f295efa0c
Georges-Lucas <georgeslucas.grondin@gmail.com>
2025-01-30 22:25:36 +0100

    ajout des différents test unitaires

--- Personnel/src/testsUnitaires/testLigue.java
+++ Personnel/src/testsUnitaires/testLigue.java
@@ -45,5 +45,62 @@ class testLigue
 		assertEquals(employe, ligue.getEmployes().first());
 	}
 	
+	@Test
+	void gatsetLigue() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Course");
+		ligue.setNom("Lancé");
+		assertEquals("Lancé", ligue.getNom());
+	}
+	
+	@Test
+	void getsetEmploye() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty",LocalDate.now(),null);
+		employe.setNom("Bruno");
+		employe.setPrenom("Brunié");
+		employe.setMail("b.brunod@gmail.com");
+		employe.setPassword("12345");
+		assertEquals("Bruno", employe.getNom());
+		assertEquals("Brunié", employe.getPrenom());
+		assertEquals("b.brunod@gmail.com", employe.getMail());
+	}
+	
+	@Test 
+	void supLigue() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		ligue.remove();
+		assertEquals(ligue, gestionPersonnel.getLigues().first());
+	}
+	
+	@Test 
+	void supemploye() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty",LocalDate.now(),null);
+		ligue.getEmployes().first().remove();
+		assertEquals(employe, ligue.getEmployes().first());
+	}
+	
+	@Test 
+	void changementAdmin() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty",LocalDate.now(),null);
+		ligue.setAdministrateur(employe);
+		assertEquals(true, ligue.getEmployes().first().estAdmin(ligue));
+	}
+	
+	@Test 
+	void supAdmin() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty",LocalDate.now(),null);
+		ligue.setAdministrateur(employe);
+		employe.remove();
+		assertEquals(employe, ligue.getEmployes().first());
+	}
 }
 	
