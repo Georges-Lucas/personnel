f1302a3ca617dc97478524398c3852905b471f86
BabskiFlorian <fbabski@infflux-opsoma.com>
2025-01-31 15:34:06 +0100

    script sql

--- Doc/Script.sql
+++ Doc/Script.sql
@@ -1,9 +1,16 @@
-CREATE TABLE ligue (
+DROP TABLE LIGUE;
+DROP TABLE EMPLOYE;
+DROP TABLE NIVEAU_ACCES;
+DROPT TABLE TRAVAIL;
+DROP TABLE ROLE;
+
+
+CREATE TABLE LIGUE (
 id_ligue int PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nom_ligue varchar(100)
 );
 
-CREATE TABLE employe (
+CREATE TABLE EMPLOYE (
 id_employe int PRIMARY KEY ,
 nom_employe varchar(100),
 prenom_employe varchar(100),
@@ -13,20 +20,22 @@ date_arrive DATE,
 date_depart DATE
 );
 
-CREATE TABLE niveau_acces (
+CREATE TABLE NIVEAU_ACCES (
 niveau_acces varchar(32) PRIMARY KEY
 );
 
-CREATE TABLE travail (
+CREATE TABLE TRAVAIL (
 id_employe int,
 id_ligue int,
-FOREIGN KEY (id_employe) REFERENCES employe(id_employe),
-FOREIGN KEY (id_ligue) REFERENCES ligue(id_ligue)
+FOREIGN KEY (id_employe) REFERENCES EMPLOYE(id_employe),
+FOREIGN KEY (id_ligue) REFERENCES LIGUE(id_ligue)
 );
 
-CREATE TABLE role (
+CREATE TABLE ROLE (
 id_employe int,
 niveau_acces varchar(32),
-FOREIGN KEY (id_employe) REFERENCES employe(id_employe),
-FOREIGN KEY (niveau_acces) REFERENCES niveau_acces(niveau_acces)
+FOREIGN KEY (id_employe) REFERENCES EMPLOYE(id_employe),
+FOREIGN KEY (niveau_acces) REFERENCES NIVEAU_ACCES(niveau_acces)
 );
+
+
