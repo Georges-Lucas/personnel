b2f7750c6e890e07fcb5be51f95175c891d7f9b3
BabskiFlorian <fbabski@infflux-opsoma.com>
2025-02-07 11:18:45 +0100

    fichier sql4

--- Doc/Script.sql
+++ Doc/Script.sql
@@ -1,7 +1,6 @@
 DROP TABLE LIGUE;
 DROP TABLE EMPLOYE;
 DROP TABLE NIVEAU_ACCES;
-DROP TABLE ROLE;
 
 
 CREATE TABLE LIGUE (
@@ -17,22 +16,15 @@ email_employe varchar(100),
 mdp_employe varchar (32),
 date_arrive DATE,
 date_depart DATE,
-id_role int,
 id_ligue int,
+id_niveau_acces int,
 FOREIGN KEY (id_ligue) REFERENCES LIGUE(id_ligue),
-FOREIGN KEY (id_role) REFERENCES ROLE(id_role)
+FOREIGN KEY (id_niveau_acces) REFERENCES NIVEAU_ACCES(id_niveau_acces)
 );
 
 CREATE TABLE NIVEAU_ACCES (
 id_niveau_acces int PRIMARY KEY NOT NULL AUTO_INCREMENT,
 niveau_acces varchar(32) PRIMARY KEY
-);
-
-
-CREATE TABLE ROLE (
-id_role int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-id_niveau_acces int,
-FOREIGN KEY (id_niveau_acces) REFERENCES NIVEAU_ACCES(id_niveau_acces)
 );
 
 
